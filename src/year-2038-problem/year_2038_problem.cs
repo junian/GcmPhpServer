1cccdf900cec50d5d875857d641d9cd31c76a3c8
Junian Triajianto <author@junian.dev>
2025-09-27 06:46:38 +0700

    Fix wrong substraction

--- src/year-2038-problem/year_2038_problem.cs
+++ src/year-2038-problem/year_2038_problem.cs
@@ -5,6 +5,6 @@
 using Humanizer;
 
 var year2038Problem = DateTimeOffset.Parse("2038-01-19T03:14:08+00:00");
-var timeUntil2038Problem = DateTimeOffset.Now - year2038Problem;
+var timeUntil2038Problem = year2038Problem - DateTimeOffset.Now;
 
-Console.WriteLine($"It is {timeUntil2038Problem.Humanize()} until Year 2038 Problem.");
+Console.WriteLine($"It is {timeUntil2038Problem.Humanize()} until the Year 2038 Problem.");
