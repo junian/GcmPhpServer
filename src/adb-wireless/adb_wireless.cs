a638d3d03f6687e0d7f4efbb463f2d47675089b6
Junian Triajianto <author@junian.dev>
2025-09-28 21:54:09 +0700

    Add adb wireless scanner

---
+++ src/adb-wireless/adb_wireless.cs
@@ -0,0 +1,30 @@
+#!/usr/bin/env dotnet run
+
+#:package Zeroconf@3.7.*
+
+using Zeroconf;
+
+const string AdbWirelessService = "_adb-tls-connect._tcp.local.";
+
+var results = await
+        ZeroconfResolver.ResolveAsync(AdbWirelessService);
+
+if (results.Count <= 0)
+{
+    Console.WriteLine("No ADB Wireless device found");
+    return;
+}
+
+Console.WriteLine($"{results.Count} ADB Wireless device(s) found");
+
+var index = 1;
+foreach (var r in results)
+{
+    if (r.Services == null)
+        continue;
+
+    foreach (var s in r.Services)
+    {
+        Console.WriteLine($"[{index++}] {r.DisplayName} {r.IPAddress}:{s.Value.Port}");
+    }
+}
