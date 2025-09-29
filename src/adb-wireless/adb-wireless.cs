b2ea3ca446a0a8b5d0f3e3930a948c8b05cda5c9
Junian Triajianto <author@junian.dev>
2025-09-29 15:41:11 +0700

    Rename

--- src/adb-wireless/adb-wireless.cs
+++ src/adb-wireless/adb-wireless.cs
@@ -11,11 +11,11 @@ var results = await
 
 if (results.Count <= 0)
 {
-    Console.WriteLine("No ADB Wireless device found");
+    Console.WriteLine("No Wireless ADB device found");
     return;
 }
 
-Console.WriteLine($"{results.Count} ADB Wireless device(s) found");
+Console.WriteLine($"{results.Count} Wireless ADB device(s) found");
 
 var index = 1;
 foreach (var r in results)
@@ -24,6 +24,7 @@ foreach (var r in results)
         continue;
 
     foreach (var s in r.Services)
-        foreach(var ip in r.IPAddresses)
-            Console.WriteLine($"[{index++}] {r.DisplayName} {ip}:{s.Value.Port}");
+    {
+        Console.WriteLine($"[{index++}] {r.DisplayName} {r.IPAddress}:{s.Value.Port}");
+    }
 }
